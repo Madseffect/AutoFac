0a18636cc278de00af475a4e4d15896ad1126fa9
Madseffect <themadsk@gmail.com>
2017-08-16 13:29:52 +0200

    First commit
    Commit så Robin kan se

---
+++ AutoFac/AutoFac/PrintService2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoFac
+{
+    class PrintService2:IPrintService2
+    {
+        public void PrintSomething2(string text)
+        {
+            Console.WriteLine(text+" yay i think i figured it out");
+        }
+    }
+}
+}
---
+++ AutoFac/AutoFac/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using System.Reflection;
+
+namespace AutoFac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<PrintService>().As<IPrintService>();
+
+            var container = builder.Build();
+            var printService = container.Resolve<IPrintService>();
+            printService.PrintSomething("Hello World");
+            Console.ReadKey();
+
+            //assembly scanning
+            var dataAccess = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(dataAccess)
+       .Where(t => t.Name.EndsWith("Repository"))
+       .AsImplementedInterfaces();
+
+
+        }
+
+    }
+}
